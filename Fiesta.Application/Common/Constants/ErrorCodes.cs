fe62be60b00aa29f2b150f5cd6f24f18d772cfcd
Marian Kmotorka <52036453+MarianKmotorka@users.noreply.github.com>
2021-02-06 22:32:04 +0100

    Rewrite ErrorCodes to camelCase

--- Fiesta.Application/Common/Constants/ErrorCodes.cs
+++ Fiesta.Application/Common/Constants/ErrorCodes.cs
@@ -2,13 +2,13 @@
 {
     public class ErrorCodes
     {
-        public const string InvalidEmailAddress = nameof(InvalidEmailAddress);
-        public const string Required = nameof(Required);
-        public const string MinLength = nameof(MinLength);
-        public const string MaxLength = nameof(MaxLength);
-        public const string MustBeUnique = nameof(MustBeUnique);
-        public const string InvalidEmailOrPassword = nameof(InvalidEmailOrPassword);
-        public const string InvalidAuthProvider = nameof(InvalidAuthProvider);
-        public const string EmailIsNotVerified = nameof(EmailIsNotVerified);
+        public const string InvalidEmailAddress = "invalidEmailOrPassword";
+        public const string Required = "required";
+        public const string MinLength = "minLength";
+        public const string MaxLength = "maxLength";
+        public const string MustBeUnique = "mustBeUnique";
+        public const string InvalidEmailOrPassword = "invalidEmailOrPassword";
+        public const string InvalidAuthProvider = "invalidAuthProvider";
+        public const string EmailIsNotVerified = "emailIsNotVerified";
     }
 }
