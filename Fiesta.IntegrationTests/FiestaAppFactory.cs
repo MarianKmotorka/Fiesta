28ebde9581dd1bf06eaeece254cefa89fd9cd0be
Marian Kmotorka <52036453+MarianKmotorka@users.noreply.github.com>
2021-02-13 16:32:37 +0100

    Mock EmailService

--- Fiesta.IntegrationTests/FiestaAppFactory.cs
+++ Fiesta.IntegrationTests/FiestaAppFactory.cs
@@ -1,28 +1,67 @@
 ﻿using System.Linq;
+using Fiesta.Application.Common.Interfaces;
 using Fiesta.Infrastracture.Persistence;
 using Fiesta.WebApi;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
 
 namespace Fiesta.IntegrationTests
 {
     public class FiestaAppFactory : WebApplicationFactory<Startup>
     {
+        private IEmailService _emailServiceMock = Substitute.For<IEmailService>();
+
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             base.ConfigureWebHost(builder);
 
             builder.ConfigureServices(services =>
             {
-                var descriptor = services.SingleOrDefault(x => x.ServiceType == typeof(DbContextOptions<FiestaDbContext>));
+                ReplaceDb(services);
+                ReplaceEmailService(services);
+            })
+                .ConfigureAppConfiguration(config =>
+                {
+                    config.Add(new TestConfigSource());
+                });
+        }
 
-                if (descriptor != null)
-                    services.Remove(descriptor);
+        private void ReplaceEmailService(IServiceCollection services)
+        {
+            var descriptor = services.SingleOrDefault(x => x.ServiceType == typeof(IEmailService));
+            if (descriptor is not null)
+                services.Remove(descriptor);
 
-                services.AddDbContext<FiestaDbContext>(options => { options.UseInMemoryDatabase(FiestaDbContext.TestDbName); });
-            });
+            services.AddTransient(x => _emailServiceMock);
+        }
+
+        private void ReplaceDb(IServiceCollection services)
+        {
+            var dbContextDescriptor = services.SingleOrDefault(x => x.ServiceType == typeof(DbContextOptions<FiestaDbContext>));
+            if (dbContextDescriptor is not null)
+                services.Remove(dbContextDescriptor);
+
+            services.AddDbContext<FiestaDbContext>(options => { options.UseInMemoryDatabase(FiestaDbContext.TestDbName); });
+        }
+    }
+
+    public class TestConfigSource : IConfigurationSource
+    {
+        public IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            return new TestConfigProvider();
+        }
+    }
+
+    public class TestConfigProvider : ConfigurationProvider
+    {
+        public override void Load()
+        {
+            Set("IsTesting", "true");
         }
     }
 }
