6878cf608536c89e3b7ee162255f8aa974845908
Adam <garamone@gmail.com>
2021-02-23 10:50:28 +0100

    Added tests

--- Fiesta.IntegrationTests/Features/Auth/DeleteAccountTests.cs
+++ Fiesta.IntegrationTests/Features/Auth/DeleteAccountTests.cs
@@ -1,6 +1,9 @@
 ﻿using System.Net.Http;
 using System.Threading.Tasks;
+using Fiesta.Application.Common.Constants;
 using Fiesta.Application.Features.Auth;
+using Fiesta.IntegrationTests.Assets;
+using Fiesta.WebApi.Middleware.ExceptionHanlding;
 using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
 using Xunit;
@@ -49,19 +52,97 @@ namespace Fiesta.IntegrationTests.Features.Auth
         [Fact]
         public async Task GivenInvalidPassword_WhenDeleteAccountWithPassword_BadRequestIsReturned()
         {
+            var registerRequest = new RegisterWithEmailAndPassword.Command
+            {
+                Email = "unique@email.com",
+                FirstName = "Jozko",
+                LastName = "Javorsky",
+                Password = "Pass123"
+            };
 
+            var registerResponse = await NotAuthedClient.PostAsJsonAsync("api/auth/register", registerRequest);
+
+            var authUser = await ArrangeDb.Users.SingleAsync(x => x.Email == registerRequest.Email);
+            authUser.EmailConfirmed = true;
+            await ArrangeDb.SaveChangesAsync();
+
+            using var client = CreateClientForUser(authUser);
+
+            var deleteResponse = await client.DeleteAsync($"/api/auth/delete-account-with-password?password=BadPassword");
+
+            var errorResposne = await deleteResponse.Content.ReadAsAsync<ErrorResponse>();
+            errorResposne.Should().BeEquivalentTo(new
+            {
+                ErrorCode = "BadRequest",
+                ErrorMessage = ErrorCodes.InvalidPassword
+            });
         }
 
         [Fact]
         public async Task GivenValidCode_WhenDeleteAccountWithGoogle_AccountIsDeleted()
         {
+            var response = await NotAuthedClient.GetAsync("/api/auth/google-code-callback?code=validCode");
+            var user = await ArrangeDb.Users.SingleAsync(x => x.Email == GoogleAssets.JohnyUserInfoModel.Email);
 
+            using var client = CreateClientForUser(user);
+
+            var deleteResponse = await client.DeleteAsync($"/api/auth/delete-account-with-code?code=validCode");
+
+            deleteResponse.EnsureSuccessStatusCode();
+
+            var deletedUser = await AssertDb.Users.SingleOrDefaultAsync(x => x.Email == GoogleAssets.JohnyUserInfoModel.Email);
+            deletedUser.Should().BeNull();
+
+            var deletedFiestaUser = await AssertDb.FiestaUsers.SingleOrDefaultAsync(x => x.Email == GoogleAssets.JohnyUserInfoModel.Email);
+            deletedFiestaUser.IsDeleted.Should().BeTrue();
         }
 
         [Fact]
         public async Task GivenInvalidCode_WhenDeleteAccountWithGoogle_BadRequestIsReturned()
         {
+            var response = await NotAuthedClient.GetAsync("/api/auth/google-code-callback?code=validCode");
+            var user = await ArrangeDb.Users.SingleAsync(x => x.Email == GoogleAssets.JohnyUserInfoModel.Email);
 
+            using var client = CreateClientForUser(user);
+
+            var deleteResponse = await client.DeleteAsync($"/api/auth/delete-account-with-code?code=invalidCode");
+
+            var errorResposne = await deleteResponse.Content.ReadAsAsync<ErrorResponse>();
+            errorResposne.Should().BeEquivalentTo(new
+            {
+                ErrorCode = "BadRequest",
+                ErrorMessage = ErrorCodes.InvalidCode
+            });
+        }
+
+        [Fact]
+        public async Task GivenInvalidGoogleAccount_WhenDeleteAccountWithGoogle_BadRequestIsReturned()
+        {
+            var registerRequest = new RegisterWithEmailAndPassword.Command
+            {
+                Email = "unique@email.com",
+                FirstName = "Jozko",
+                LastName = "Javorsky",
+                Password = "Pass123"
+            };
+
+            var registerResponse = await NotAuthedClient.PostAsJsonAsync("api/auth/register", registerRequest);
+
+            var authUser = await ArrangeDb.Users.SingleAsync(x => x.Email == registerRequest.Email);
+            authUser.EmailConfirmed = true;
+            authUser.AddGoogleAuthProvider("random@email.com");
+            await ArrangeDb.SaveChangesAsync();
+
+            using var client = CreateClientForUser(authUser);
+
+            var deleteResponse = await client.DeleteAsync($"/api/auth/delete-account-with-code?code=validCode");
+
+            var errorResposne = await deleteResponse.Content.ReadAsAsync<ErrorResponse>();
+            errorResposne.Should().BeEquivalentTo(new
+            {
+                ErrorCode = "BadRequest",
+                ErrorMessage = ErrorCodes.InvalidGoogleAccount
+            });
         }
     }
 }
