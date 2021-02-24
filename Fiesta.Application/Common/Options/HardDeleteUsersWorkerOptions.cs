8dadc2325128e3985639a511ec4cabaf25097519
MarianKmotorka <52036453+MarianKmotorka@users.noreply.github.com>
2021-02-24 16:40:55 +0100

    Background worker for deleting users marked as deleted (#16)

---
+++ Fiesta.Application/Common/Options/HardDeleteUsersWorkerOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Fiesta.Application.Common.Options
+{
+    public class HardDeleteUsersWorkerOptions
+    {
+        public TimeSpan PollingPeriod { get; set; } = TimeSpan.FromDays(1);
+
+        public bool Enabled { get; set; }
+    }
+}
---
+++ Fiesta.IntegrationTests/Features/Users/HardDeleteUsersTests.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Fiesta.Application.Features.Users;
+using Fiesta.Domain.Entities.Users;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace Fiesta.IntegrationTests.Features.Users
+{
+    [Collection(nameof(FiestaAppFactory))]
+    public class HardDeleteUsersTests : WebAppTestBase
+    {
+        private HardDeleteUsers.Handler _sut;
+
+        public HardDeleteUsersTests(FiestaAppFactory factory) : base(factory)
+        {
+            _sut = new HardDeleteUsers.Handler(ActDb);
+        }
+
+        [Fact]
+        public async Task GivenSomeUsersMarkedAsDeleted_WhenHandlerIsCalled_OnlyMarkedUsersAreDeleted()
+        {
+            ArrangeDb.FiestaUsers.Add(new FiestaUser("deleted@email.com") { IsDeleted = true });
+            var notDeletedUser = ArrangeDb.FiestaUsers.Add(new FiestaUser("NOTdeleted@email.com")).Entity;
+            await ArrangeDb.SaveChangesAsync();
+
+            await _sut.Handle(new HardDeleteUsers.Command(), default);
+
+            var userDb = await AssertDb.FiestaUsers.SingleAsync();
+            userDb.Id.Should().Be(notDeletedUser.Id);
+        }
+    }
+}
