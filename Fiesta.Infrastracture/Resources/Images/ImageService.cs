b61e8d25d67a42549de09f319d78d7f2575a2c22
Adam <garamone@gmail.com>
2021-03-09 18:52:58 +0100

    Moved get signature to service

---
+++ Fiesta.Infrastracture/Resources/Images/ImageService.cs
@@ -0,0 +1,47 @@
+﻿using CloudinaryDotNet;
+using Fiesta.Application.Common.Options;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Fiesta.Infrastracture.Resources.Images
+{
+    public class ImageService
+    {
+        private readonly CloudinaryOptions _cloudinaryOptions;
+
+        public ImageService(CloudinaryOptions cloudinaryOptions)
+        {
+            _cloudinaryOptions = cloudinaryOptions;
+        }
+
+        private SignatureResponse GenerateCloudinarySignature()
+        {
+            var timestamp = Stopwatch.GetTimestamp().ToString();
+
+            var cloudinary = new Cloudinary(
+                new Account(
+                    _cloudinaryOptions.CloudName,
+                    _cloudinaryOptions.ApiKey,
+                    _cloudinaryOptions.ApiSecret
+                    )
+                );
+
+            var parameters = new Dictionary<string, object>() { ["timestamp"] = timestamp };
+            var signature = cloudinary.Api.SignParameters(parameters);
+
+            return new SignatureResponse
+            {
+                Timestamp = timestamp,
+                Signature = signature
+            };
+
+        }
+
+        public class SignatureResponse
+        {
+            public string Timestamp { get; set; }
+
+            public string Signature { get; set; }
+        }
+    }
+}
