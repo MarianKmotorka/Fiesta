6b611551c6385da76514c714a65ad773d77a9de3
Adam <garamone@gmail.com>
2021-03-10 22:45:18 +0100

    Added tests class

---
+++ Fiesta.IntegrationTests/Features/Users/UpdateUserTests.cs
@@ -0,0 +1,13 @@
+﻿using Fiesta.IntegrationTests;
+using Xunit;
+
+namespace Fiesta.WebApi.Tests.Features.Users
+{
+    [Collection(nameof(FiestaAppFactory))]
+    public class UpdateUserTests : WebAppTestBase
+    {
+        public UpdateUserTests(FiestaAppFactory factory) : base(factory)
+        {
+        }
+    }
+}
