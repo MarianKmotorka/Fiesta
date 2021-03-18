cc3e895668074fa18d5155e73912b1df849c6532
Marian Kmotorka <Marian.Kmotorka@quest.com>
2021-03-18 21:34:18 +0100

    Optional

---
+++ src/Fiesta.Application/Common/Validators/IRuleBuilderInitialOptional`2.cs
@@ -0,0 +1,10 @@
+﻿using FluentValidation;
+using FluentValidation.Internal;
+
+namespace Fiesta.Application.Common.Validators
+{
+    public interface IRuleBuilderInitialOptional<T, TElement>
+       : IRuleBuilder<T, TElement>, IConfigurable<OptionalPropertyRule<T, TElement>, IRuleBuilderInitialOptional<T, TElement>>, IRuleBuilderInitial<T, TElement>
+    {
+    }
+}
