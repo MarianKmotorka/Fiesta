17a1035744c0d95a524ab99293069448b4ef652c
Adam <garamone@gmail.com>
2021-03-27 15:58:37 +0100

    Added friend tests

---
+++ tests/Fiesta.IntegrationTests/Features/Users/Friends/AddFriendTests.cs
@@ -0,0 +1,101 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Fiesta.Application.Common.Constants;
+using Fiesta.Domain.Entities.Users;
+using Fiesta.IntegrationTests;
+using Fiesta.WebApi.Middleware.ExceptionHanlding;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using TestBase.Assets;
+using Xunit;
+
+namespace Fiesta.WebApi.Tests.Features.Users.Friends
+{
+    [Collection(nameof(FiestaAppFactory))]
+    public class AddFriendTests : WebAppTestBase
+    {
+        public AddFriendTests(FiestaAppFactory factory) : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task GivenValidRequest_WhenConfirmingFriendRequest_FriendIsAdded()
+        {
+            var (authFriend, fiestaFriend) = ArrangeDb.SeedBasicUser();
+            var fiestaUser = await ArrangeDb.FiestaUsers.FindAsync(LoggedInUserId);
+
+            await ArrangeDb.FriendRequests.AddAsync(new FriendRequest(fiestaUser, fiestaFriend));
+            await ArrangeDb.SaveChangesAsync();
+
+            using var client = CreateClientForUser(authFriend);
+
+            var response = await client.PostAsJsonAsync("api/friends/confirm-request", new { friendId = LoggedInUserId });
+
+            response.EnsureSuccessStatusCode();
+
+            var friendRequest = await AssertDb.FriendRequests.SingleOrDefaultAsync(x => x.FromId == LoggedInUserId && x.ToId == fiestaFriend.Id);
+
+            friendRequest.Should().BeNull();
+
+            var friendShip1 = await AssertDb.UserFriends.SingleOrDefaultAsync(x => x.UserId == LoggedInUserId && x.FriendId == fiestaFriend.Id);
+            var friendShip2 = await AssertDb.UserFriends.SingleOrDefaultAsync(x => x.UserId == fiestaFriend.Id && x.FriendId == LoggedInUserId);
+
+            friendShip1.Should().NotBeNull();
+            friendShip2.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task GivenInvalidId_WhenConfirmingFriendRequest_BadRequestIsReturned()
+        {
+            var (authFriend, fiestaFriend) = ArrangeDb.SeedBasicUser();
+            var fiestaUser = await ArrangeDb.FiestaUsers.FindAsync(LoggedInUserId);
+
+            await ArrangeDb.FriendRequests.AddAsync(new FriendRequest(fiestaUser, fiestaFriend));
+            await ArrangeDb.SaveChangesAsync();
+
+            using var client = CreateClientForUser(authFriend);
+
+            var response = await client.PostAsJsonAsync("api/friends/confirm-request", new { friendId = "InvalidId" });
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+            var errorResposne = await response.Content.ReadAsAsync<ErrorResponse>();
+            errorResposne.Should().BeEquivalentTo(new
+            {
+                ErrorCode = "ValidationError",
+                ErrorDetails = new object[]
+                {
+                    new
+                    {
+                        Code = ErrorCodes.DoesNotExist,
+                        PropertyName="friendId"
+                    }
+                }
+            });
+        }
+
+        [Fact]
+        public async Task GivenSenderConfirmingRequest_WhenConfirmingFriendRequest_BadRequestIsReturned()
+        {
+            var (authFriend, fiestaFriend) = ArrangeDb.SeedBasicUser();
+            var fiestaUser = await ArrangeDb.FiestaUsers.FindAsync(LoggedInUserId);
+
+            await ArrangeDb.FriendRequests.AddAsync(new FriendRequest(fiestaUser, fiestaFriend));
+            await ArrangeDb.SaveChangesAsync();
+
+            using var client = CreateClientForUser(authFriend);
+
+            var response = await Client.PostAsJsonAsync("api/friends/confirm-request", new { friendId = fiestaFriend.Id });
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+            var errorResposne = await response.Content.ReadAsAsync<ErrorResponse>();
+            errorResposne.Should().BeEquivalentTo(new
+            {
+                ErrorCode = "BadRequest",
+                ErrorMessage = "Friend request does not exist",
+            });
+        }
+    }
+}
---
+++ tests/Fiesta.IntegrationTests/Features/Users/Friends/DeleteFriendTests.cs
@@ -0,0 +1,126 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Fiesta.Application.Common.Constants;
+using Fiesta.Domain.Entities.Users;
+using Fiesta.IntegrationTests;
+using Fiesta.WebApi.Middleware.ExceptionHanlding;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using TestBase.Assets;
+using Xunit;
+
+namespace Fiesta.WebApi.Tests.Features.Users.Friends
+{
+    [Collection(nameof(FiestaAppFactory))]
+    public class DeleteFriendTests : WebAppTestBase
+    {
+        public DeleteFriendTests(FiestaAppFactory factory) : base(factory)
+        {
+        }
+
+        [Fact]
+        public async Task GivenValidRequest_WhenDeletingFriendFromOneSide_FriendIsDeleted()
+        {
+            var (_, fiestaFriend) = ArrangeDb.SeedBasicUser();
+            var fiestaUser = await ArrangeDb.FiestaUsers.FindAsync(LoggedInUserId);
+
+            await ArrangeDb.UserFriends.AddAsync(new UserFriend(fiestaUser, fiestaFriend));
+            await ArrangeDb.UserFriends.AddAsync(new UserFriend(fiestaFriend, fiestaUser));
+            await ArrangeDb.SaveChangesAsync();
+
+            var response = await Client.DeleteAsync($"api/friends/{fiestaFriend.Id}");
+
+            response.EnsureSuccessStatusCode();
+
+            var friendShip1 = await AssertDb.UserFriends.SingleOrDefaultAsync(x => x.UserId == LoggedInUserId && x.FriendId == fiestaFriend.Id);
+            var friendShip2 = await AssertDb.UserFriends.SingleOrDefaultAsync(x => x.UserId == fiestaFriend.Id && x.FriendId == LoggedInUserId);
+
+            friendShip1.Should().BeNull();
+            friendShip2.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task GivenInvalidId_WhenDeletingFriendFromOneSide_BadRequestIsReturned()
+        {
+            var (_, fiestaFriend) = ArrangeDb.SeedBasicUser();
+            var fiestaUser = await ArrangeDb.FiestaUsers.FindAsync(LoggedInUserId);
+
+            await ArrangeDb.UserFriends.AddAsync(new UserFriend(fiestaUser, fiestaFriend));
+            await ArrangeDb.UserFriends.AddAsync(new UserFriend(fiestaFriend, fiestaUser));
+            await ArrangeDb.SaveChangesAsync();
+
+            var response = await Client.DeleteAsync($"api/friends/InvalidId");
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+            var errorResposne = await response.Content.ReadAsAsync<ErrorResponse>();
+            errorResposne.Should().BeEquivalentTo(new
+            {
+                ErrorCode = "ValidationError",
+                ErrorDetails = new object[]
+                {
+                    new
+                    {
+                        Code = ErrorCodes.DoesNotExist,
+                        PropertyName="friendId"
+                    }
+                }
+            });
+        }
+
+        [Fact]
+        public async Task GivenValidRequest_WhenDeletingFriendFromOtherSide_FriendIsDeleted()
+        {
+            var (authFriend, fiestaFriend) = ArrangeDb.SeedBasicUser();
+            var fiestaUser = await ArrangeDb.FiestaUsers.FindAsync(LoggedInUserId);
+
+            await ArrangeDb.UserFriends.AddAsync(new UserFriend(fiestaUser, fiestaFriend));
+            await ArrangeDb.UserFriends.AddAsync(new UserFriend(fiestaFriend, fiestaUser));
+            await ArrangeDb.SaveChangesAsync();
+
+            using var client = CreateClientForUser(authFriend);
+
+            var response = await client.DeleteAsync($"api/friends/{LoggedInUserId}");
+
+            response.EnsureSuccessStatusCode();
+
+            var friendShip1 = await AssertDb.UserFriends.SingleOrDefaultAsync(x => x.UserId == LoggedInUserId && x.FriendId == fiestaFriend.Id);
+            var friendShip2 = await AssertDb.UserFriends.SingleOrDefaultAsync(x => x.UserId == fiestaFriend.Id && x.FriendId == LoggedInUserId);
+
+            friendShip1.Should().BeNull();
+            friendShip2.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task GivenInvalidId_WhenDeletingFriendFromOtherSide_BadRequestIsReturned()
+        {
+            var (authFriend, fiestaFriend) = ArrangeDb.SeedBasicUser();
+            var fiestaUser = await ArrangeDb.FiestaUsers.FindAsync(LoggedInUserId);
+
+            await ArrangeDb.UserFriends.AddAsync(new UserFriend(fiestaUser, fiestaFriend));
+            await ArrangeDb.UserFriends.AddAsync(new UserFriend(fiestaFriend, fiestaUser));
+            await ArrangeDb.SaveChangesAsync();
+
+            using var client = CreateClientForUser(authFriend);
+
+            var response = await client.DeleteAsync($"api/friends/InvalidId");
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+
+            var errorResposne = await response.Content.ReadAsAsync<ErrorResponse>();
+            errorResposne.Should().BeEquivalentTo(new
+            {
+                ErrorCode = "ValidationError",
+                ErrorDetails = new object[]
+                {
+                    new
+                    {
+                        Code = ErrorCodes.DoesNotExist,
+                        PropertyName="friendId"
+                    }
+                }
+            });
+        }
+    }
+}
