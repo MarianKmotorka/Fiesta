44ade24ac4f9fdb28261aaea0800877dc3b532ce
Adam <garamone@gmail.com>
2021-04-11 21:16:05 +0200

    Fixed hard delete (#43)

--- src/Fiesta.Application/Features/Users/HardDeleteUsers.cs
+++ src/Fiesta.Application/Features/Users/HardDeleteUsers.cs
@@ -25,9 +25,19 @@ namespace Fiesta.Application.Features.Users
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
                 var deletedUsers = await _db.FiestaUsers.IgnoreQueryFilters().Where(x => x.IsDeleted).ToListAsync(cancellationToken);
-                _db.FiestaUsers.RemoveRange(deletedUsers);
 
+                foreach (var deletedUser in deletedUsers)
+                {
+                    var friends = await _db.UserFriends.IgnoreQueryFilters().Where(x => x.UserId == deletedUser.Id || x.FriendId == deletedUser.Id).ToListAsync(cancellationToken);
+                    _db.UserFriends.RemoveRange(friends);
+
+                    var friendRequests = await _db.FriendRequests.IgnoreQueryFilters().Where(x => x.ToId == deletedUser.Id || x.FromId == deletedUser.Id).ToListAsync(cancellationToken);
+                    _db.FriendRequests.RemoveRange(friendRequests);
+                }
+
+                _db.FiestaUsers.RemoveRange(deletedUsers);
                 await _db.SaveChangesAsync(cancellationToken);
+
                 return Unit.Value;
             }
         }
