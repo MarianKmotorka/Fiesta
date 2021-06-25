5f4ebdcb3af897aa111587022794b8ce474911a0
Marian Kmotorka <Marian.Kmotorka@quest.com>
2021-06-25 10:05:13 +0200

    notifications

--- src/Fiesta.Application/Features/Users/Friends/DeleteFriend.cs
+++ src/Fiesta.Application/Features/Users/Friends/DeleteFriend.cs
@@ -2,8 +2,13 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Fiesta.Application.Common.Interfaces;
+using Fiesta.Application.Features.Common;
+using Fiesta.Application.Features.Notifications;
+using Fiesta.Application.Models.Notifications;
 using Fiesta.Application.Utils;
+using Fiesta.Domain.Entities.Notifications;
 using MediatR;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 
 namespace Fiesta.Application.Features.Users.Friends
@@ -20,16 +25,22 @@ namespace Fiesta.Application.Features.Users.Friends
         public class Handler : IRequestHandler<Command, Unit>
         {
             private readonly IFiestaDbContext _db;
+            private readonly IHubContext<NotificationsHub, INotificationsClient> _notificationsHub;
 
-            public Handler(IFiestaDbContext db)
+            public Handler(IFiestaDbContext db, IHubContext<NotificationsHub, INotificationsClient> notificationsHub)
             {
                 _db = db;
+                _notificationsHub = notificationsHub;
             }
 
             public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
             {
                 var fiestaUser = await _db.FiestaUsers.Include(x => x.Friends).SingleOrNotFoundAsync(x => x.Id == request.UserId, cancellationToken);
                 var fiestaFriend = await _db.FiestaUsers.Include(x => x.Friends).SingleOrNotFoundAsync(x => x.Id == request.FriendId, cancellationToken);
+
+                var notiifcationModel = new FriendRemoved(fiestaUser);
+                var notification = _db.Notifications.Add(new Notification(fiestaFriend.Id, notiifcationModel)).Entity;
+                await _notificationsHub.Notify(notification);
 
                 fiestaUser.RemoveFriend(fiestaFriend);
                 await _db.SaveChangesAsync(cancellationToken);
