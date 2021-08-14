779ba0b894bf30f6f4f00b40b6a11eccb9cd0db7
MarianKmotorka <52036453+MarianKmotorka@users.noreply.github.com>
2021-08-14 18:19:25 +0200

    Endpoints for attended and organized events (#55)
    * Get organized events
    
    * Get attended events

--- src/Fiesta.WebApi/Controllers/UsersController.cs
+++ src/Fiesta.WebApi/Controllers/UsersController.cs
@@ -76,10 +76,23 @@ namespace Fiesta.WebApi.Controllers
         }
 
         [Authorize(nameof(FiestaRoleEnum.BasicUser))]
-        [HttpPost("{id}/events/query")]
-        public async Task<ActionResult<QueryResponse<GetUserEvents.EventDto>>> GetUserEvents(string id, GetUserEvents.Query query, CancellationToken cancellationToken)
+        [HttpPost("{id}/attended-events")]
+        public async Task<ActionResult<QueryResponse<EventDto>>> GetUserEvents(string id, GetUserAttendedEvents.Query query, CancellationToken cancellationToken)
         {
             query.UserId = id;
+            query.CurrentUserId = CurrentUserService.UserId;
+            query.Role = CurrentUserService.Role;
+            var response = await Mediator.Send(query, cancellationToken);
+            return Ok(response);
+        }
+
+        [Authorize(nameof(FiestaRoleEnum.BasicUser))]
+        [HttpPost("{id}/organized-events")]
+        public async Task<ActionResult<QueryResponse<EventDto>>> GetOrganizedEvents(string id, GetUserOrganizedEvents.Query query, CancellationToken cancellationToken)
+        {
+            query.UserId = id;
+            query.CurrentUserId = CurrentUserService.UserId;
+            query.Role = CurrentUserService.Role;
             var response = await Mediator.Send(query, cancellationToken);
             return Ok(response);
         }
