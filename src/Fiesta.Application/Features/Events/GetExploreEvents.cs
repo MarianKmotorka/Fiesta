6e828715b71fb2b42138a61305a27eebf391dea0
Marian Kmotorka <Marian.Kmotorka@quest.com>
2021-08-28 18:42:34 +0200

    Explore events

---
+++ src/Fiesta.Application/Features/Events/GetExploreEvents.cs
@@ -0,0 +1,74 @@
+﻿using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading;
+using System.Threading.Tasks;
+using Fiesta.Application.Common.Interfaces;
+using Fiesta.Application.Common.Queries;
+using Fiesta.Application.Features.Common;
+using Fiesta.Domain.Entities.Events;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fiesta.Application.Features.Events
+{
+    /// <summary>
+    /// Returns events where user is not attandee nor organizer
+    /// </summary>
+    public class GetExploreEvents
+    {
+        public class Query : IRequest<QueryResponse<ResponseDto>>
+        {
+            [JsonIgnore]
+            public string CurrentUserId { get; set; }
+
+            public QueryDocument QueryDocument { get; set; } = new();
+        }
+
+        public class Handler : IRequestHandler<Query, QueryResponse<ResponseDto>>
+        {
+            private readonly IFiestaDbContext _db;
+
+            public Handler(IFiestaDbContext db)
+            {
+                _db = db;
+            }
+
+            public async Task<QueryResponse<ResponseDto>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                return await _db.Events.AsNoTracking()
+                    .Where(x => x.Attendees.All(x => x.AttendeeId != request.CurrentUserId))
+                    .Where(x => x.OrganizerId != request.CurrentUserId)
+                    .Where(x => x.AccessibilityType == AccessibilityType.Public ||
+                               (x.AccessibilityType == AccessibilityType.FriendsOnly && x.Attendees.Any(a => a.Attendee.Friends.Any(f => f.FriendId == request.CurrentUserId))))
+                    .Select(x => new ResponseDto
+                    {
+                        Id = x.Id,
+                        AccessibilityType = x.AccessibilityType,
+                        StartDate = x.StartDate,
+                        EndDate = x.EndDate,
+                        Name = x.Name,
+                        Description = x.Description,
+                        BannerUrl = x.BannerUrl,
+                        OrganizerPictureUrl = x.Organizer.PictureUrl,
+                        City = x.Location.City,
+                        State = x.Location.State,
+                        Capacity = x.Capacity,
+                        AttendeesCount = x.Attendees.Count(),
+                    })
+                    .OrderBy(x => x.StartDate)
+                    .BuildResponse(request.QueryDocument, cancellationToken);
+            }
+        }
+
+        public class ResponseDto : EventDto
+        {
+            public string OrganizerPictureUrl { get; set; }
+
+            public string Description { get; set; }
+
+            public int Capacity { get; set; }
+
+            public int AttendeesCount { get; set; }
+        }
+    }
+}
--- src/Fiesta.WebApi/Controllers/EventsController.cs
+++ src/Fiesta.WebApi/Controllers/EventsController.cs
@@ -160,5 +160,13 @@ namespace Fiesta.WebApi.Controllers
             }, cancellationToken);
             return Ok(result);
         }
+
+        [HttpPost("explore")]
+        public async Task<ActionResult<QueryResponse<UserDto>>> GetExploreEvents(GetExploreEvents.Query request, CancellationToken cancellationToken)
+        {
+            request.CurrentUserId = CurrentUserService.UserId;
+            var result = await Mediator.Send(request, cancellationToken);
+            return Ok(result);
+        }
     }
 }
